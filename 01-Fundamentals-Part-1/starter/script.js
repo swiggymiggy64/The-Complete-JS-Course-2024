41e9625382c8e4597c8c37356f0a8eb43dc1a12b
Michael Williams <104996526+swiggymiggy64@users.noreply.github.com>
2025-10-22 13:01:19 +0100

    First Commit!

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,68 @@
+// * Linking a JS File
+
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// * Values & Variables
+
+// let firstname = "Jonas";
+console.log(firstname);
+
+// . Assignment
+const country = "UK";
+const continent = "Europe";
+let population = 10000000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// * Data Types
+
+// Object
+const person = {
+  name: "Jonas",
+};
+
+// Primitive
+let firstname = "Jonas";
+// let age = 30;
+
+const boolExample = false;
+const intExample = 10; // in JS these are both a "number" value
+const floatExample = 1.0; // in JS these are both a "number" value
+const stringExample = "Hello World!";
+const undefinedExample = undefined;
+let undefinedExample2;
+const nullExample = null;
+
+console.log(typeof boolExample);
+
+// . A
+const isIsland = True;
+let language;
+console.log(isIsland);
+
+// * let, const & var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990 // Wont work, cant reassign a const
+// const job; // Wont work, need to define a value for a const
+
+// var is old, dont use it
+var job = "programmer";
+job = "teacher";
+
+lastName = "Jones"; // This works but dont do it, as JS will make this a global variable
+console.log(lastName);
+
+// . A
+language = "English";
+population = "20000000";
+console.log(population);
+
+// * Basic Operators
+
+// . A
