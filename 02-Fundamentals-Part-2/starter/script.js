3065ad718c12b29964b72969a66803dbcc3fe73c
Michael Williams <104996526+swiggymiggy64@users.noreply.github.com>
2025-10-27 20:21:45 +0000

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -275,3 +275,151 @@
 // console.log(neighbours);
 
 // ! Challenge
+
+// const tips = [];
+// const total = [];
+// const bills = [calcTip(125), calcTip(555), calcTip(44)];
+
+// function calcTip(bill) {
+//   if (bill >= 50 && bill <= 300) {
+//     tips.push(bill * 0.15);
+//     total.push(bill * 0.15 + bill);
+//   } else {
+//     tips.push(bill * 0.2);
+//     total.push(bill * 0.2 + bill);
+//   }
+// }
+
+// console.log(tips);
+// console.log(total);
+
+// * Introduction to Objects
+
+// const jonasArray = [
+//   "Jonas",
+//   "Schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+// ];
+
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+
+// . A
+
+// const myCountry = {
+//   country: "UK",
+//   capital: "London",
+//   language: "Inglish. Innit",
+//   population: 22000000,
+//   neighbours: null,
+// };
+
+// * Dot vs. Bracket Notation
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+// console.log(jonas["lastName"]);
+
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
+
+// // console.log(jonas.'last' + nameKey)
+
+// // const interestedIn = prompt(
+// //   "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+// // );
+
+// const interestedIn = "job";
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     "Wrong request! Choose between firstName, lastName, age, job, and friends"
+//   );
+// }
+
+// jonas.location = "Portugal";
+// jonas["twitter"] = "@jonasschmedtman";
+// console.log(jonas);
+
+// // Challenge
+// console.log(
+//   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+// );
+
+// . A
+
+// const myCountry = {
+//   country: "UK",
+//   capital: "London",
+//   language: "Inglish innit",
+//   population: 22000000,
+//   neighbours: 0,
+// };
+
+// console.log(
+//   `${myCountry.country} has ${myCountry.population} ${myCountry.language} people, ${myCountry.neighbours} neighbours & a capital called ${myCountry.capital} fam.`
+// );
+
+// myCountry.population += 2000000;
+// myCountry.population -= 2000000;
+// console.log(myCountry);
+
+// * Object Methods
+
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   birthYeah: 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+//   hasDriversLicense: true,
+
+//   // calcAge: function (birthYeah) {
+//   //   return 2037 - birthYeah;
+//   // }
+
+//   // calcAge: function () {
+//   //   // console.log(this);
+//   //   return 2037 - this.birthYeah;
+//   // }
+
+//   calcAge: function () {
+//     this.age = 2037 - this.birthYeah;
+//     return this.age;
+//   },
+
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.calcAge()}-year old ${
+//       jonas.job
+//     }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+//   },
+// };
+
+// console.log(jonas.calcAge());
+
+// console.log(jonas.age);
+
+// // Lesson Challenge
+
+// console.log(jonas.getSummary());
+
+// . A
+
+// ! Challenge
