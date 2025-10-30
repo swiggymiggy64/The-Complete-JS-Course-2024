bc37c91b9d2bc6ac3e23d62691b76c8ba3a6fdcb
Michael Williams <104996526+swiggymiggy64@users.noreply.github.com>
2025-10-30 16:41:30 +0000

    up

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,45 +1,69 @@
-'use strict';
+"use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
-let messageElement = document.querySelector('.message');
-let numberElement = document.querySelector('.number');
-let scoreElement = document.querySelector('.score');
-let guessElement = document.querySelector('.guess');
-let checkBtnElement = document.querySelector('.check');
+let messageElement = document.querySelector(".message");
+let numberElement = document.querySelector(".number");
+let scoreElement = document.querySelector(".score");
+let highscoreElement = document.querySelector(".highscore");
+let guessElement = document.querySelector(".guess");
+let checkBtnElement = document.querySelector(".check");
+let againBtnElement = document.querySelector(".again");
+let bodyElement = document.querySelector("body");
 
 // messageElement.textContent = 'Correct Number!';
-numberElement.textContent = secretNumber;
+// numberElement.textContent = secretNumber; // Show the winning number
 // scoreElement.textContent = 10;
 // guessElement.value = 23;
 
-checkBtnElement.addEventListener('click', handleClick);
+const displayMessage = function (message) {
+  messageElement.textContent = message;
+};
 
-function handleClick() {
+checkBtnElement.addEventListener("click", handleCheck);
+againBtnElement.addEventListener("click", handleAgain);
+
+function handleCheck() {
   // console.log(Number(guessElement.value));
   const guessValue = Number(guessElement.value);
-  if (!guessValue) messageElement.textContent = 'No number!';
+  // Guard
+  if (!guessValue) {
+    displayMessage("No number!");
+  }
+  // Win
+  else if (guessValue === secretNumber) {
+    displayMessage("Correct Number! 🎉");
+    bodyElement.style.backgroundColor = "green";
+    numberElement.style.width = "30rem";
+    numberElement.textContent = secretNumber;
 
-  if (guessValue === secretNumber) {
-    messageElement.textContent = 'Correct Number! 🎉';
-  } else if (guessValue > secretNumber) {
-    if (score > 0) {
-      messageElement.textContent = 'Too high!';
-      score--;
-      scoreElement.textContent = score;
-    } else {
-      messageElement.textContent = 'You lost!';
+    // Set highscore
+    if (score > highscore) {
+      highscore = score;
+      highscoreElement.textContent = highscore;
     }
-  } else if (guessValue < secretNumber) {
-    if (score > 0) {
-      messageElement.textContent = 'Too low!';
+  }
+  // When guess is wrong
+  else if (guessValue !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guessValue > secretNumber ? "Too high!" : "Too low!");
       score--;
       scoreElement.textContent = score;
     } else {
-      messageElement.textContent = 'You lost!';
+      displayMessage("You lost!");
     }
   }
 }
 
-// * 008 Manipulating CSS Styles
+function handleAgain() {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  numberElement.textContent = "?";
+  displayMessage("Start guessing...");
+  score = 20;
+  scoreElement.textContent = score;
+  guessElement.value = "";
+  bodyElement.style.backgroundColor = "#222";
+  numberElement.style.width = "15rem";
+}
