c6d5e8fe03d7f4d4de8e2a563135071d97a01aa6
Michael Williams <104996526+swiggymiggy64@users.noreply.github.com>
2025-12-07 10:01:49 +0000

    Update script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -220,3 +220,48 @@ btn.addEventListener('click', function () {
 // getCountryData('odaiwuhdowad');
 
 // ! Coding Challenge 1
+
+// const whereAmI = function (lat, lng) {
+//   fetch(
+//     `https://geocode.xyz/${lat},${lng}?geoit=json&auth=524303394004707782469x78139`
+//   )
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}`);
+//       // getCountryData(data.country);
+//       return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Country not found (${res.status})`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log('data', data);
+//       renderCountry(data[0]);
+//     })
+//     .catch(err => {
+//       console.log(`Oops! Something went wrong: ${err.message}`);
+//     });
+// };
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+// * The Event Loop in Practice
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 5000000000; i++) {}
+//   console.log(res);
+// });
+// console.log('Test end');
+
+// * Building a Simple Promise
